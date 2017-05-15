8e4538c9334f7d2ce11c6ffd67756cff04cac362
Jean-Damien Durand <jeandamiendurand@free.fr>
2017-05-15 06:15:22 +0200

    dzil xtest

--- lib/MarpaX/ESLIF/ECMA404/RecognizerInterface.pm
+++ lib/MarpaX/ESLIF/ECMA404/RecognizerInterface.pm
@@ -9,22 +9,115 @@ package MarpaX::ESLIF::ECMA404::RecognizerInterface;
 
 # AUTHORITY
 
+=head1 DESCRIPTION
+
+MarpaX::ESLIF::ECMA404's Recognizer Interface
+
+=head1 SYNOPSIS
+
+    use MarpaX::ESLIF::ECMA404::RecognizerInterface;
+
+    my $recognizerInterface = MarpaX::ESLIF::ECMA404::RecognizerInterface->new();
+
+=cut
+
+
 # -----------
 # Constructor
 # -----------
+
+=head1 SUBROUTINES/METHODS
+
+=head2 new($class, $string)
+
+Instantiate a new recognizer interface object. Argument is the data.
+
+=cut
+
 sub new { bless \$_[1], $_[0] }
 
 # ----------------
 # Required methods
 # ----------------
+
+=head2 Required methods
+
+=head3 read($self)
+
+Returns a true or a false value, indicating if last read was successful. Default is a true value.
+
+=cut
+
 sub read                   {        1 } # First read callback will be ok
+
+=head3 isEof($self)
+
+Returns a true or a false value, indicating if end-of-data is reached. Default is a true value.
+
+=cut
+
 sub isEof                  {        1 } # ../. and we will say this is EOF
+
+=head3 isCharacterStream($self)
+
+Returns a true or a false value, indicating if last read is a stream of characters. Default is a true value.
+
+=cut
+
 sub isCharacterStream      {        1 } # MarpaX::ESLIF will validate the input
+
+=head3 encoding($self)
+
+Returns encoding information. Default is undef.
+
+=cut
+
 sub encoding               {          } # Let MarpaX::ESLIF guess
+
+=head3 data($self)
+
+Returns last bunch of data. Default is the string passed in the constructor.
+
+=cut
+
 sub data                   { ${$_[0]} } # Data itself
+
+=head3 isWithDisableThreshold($self)
+
+Returns a true or a false value, indicating if threshold warning is on or off, respectively. Default is a false value.
+
+=cut
+
 sub isWithDisableThreshold {        0 } # Disable threshold warning ?
+
+=head3 isWithExhaustion($self)
+
+Returns a true or a false value, indicating if exhaustion event is on or off, respectively. Default is a false value.
+
+=cut
+
 sub isWithExhaustion       {        0 } # Exhaustion event ?
+
+=head3 isWithNewline($self)
+
+Returns a true or a false value, indicating if newline count is on or off, respectively. Default is a false value.
+
+=cut
+
 sub isWithNewline          {        0 } # Newline count ?
+
+=head3 isWithTrack($self)
+
+Returns a true or a false value, indicating if absolute position tracking is on or off, respectively. Default is a false value.
+
+=cut
+
 sub isWithTrack            {        0 } # Absolute position tracking ?
+
+=head1 SEE ALSO
+
+L<MarpaX::ESLIF::ECMA404>
+
+=cut
 
 1;
--- lib/MarpaX/ESLIF/ECMA404/ValueInterface.pm
+++ lib/MarpaX/ESLIF/ECMA404/ValueInterface.pm
@@ -9,6 +9,18 @@ package MarpaX::ESLIF::ECMA404::ValueInterface;
 
 # AUTHORITY
 
+=head1 DESCRIPTION
+
+MarpaX::ESLIF::ECMA404's Value Interface
+
+=head1 SYNOPSIS
+
+    use MarpaX::ESLIF::ECMA404::ValueInterface;
+
+    my $valueInterface = MarpaX::ESLIF::ECMA404::ValueInterface->new();
+
+=cut
+
 my $_BACKSPACE = chr(0x0008);
 my $_FORMFEED  = chr(0x000C);
 my $_NEWLINE   = chr(0x000A);
@@ -18,27 +30,147 @@ my $_TAB       = chr(0x0009);
 # -----------
 # Constructor
 # -----------
+
+=head1 SUBROUTINES/METHODS
+
+=head2 new($class)
+
+Instantiate a new value interface object.
+
+=cut
+
 sub new                { bless [], $_[0] }
 
 # ----------------
 # Required methods
 # ----------------
+
+=head2 Required methods
+
+=head3 isWithHighRankOnly($self)
+
+Returns a true or a false value, indicating if valuation should use highest ranked rules or not, respectively. Default is a true value.
+
+=cut
+
 sub isWithHighRankOnly { 1 }  # When there is the rank adverb: highest ranks only ?
+
+=head3 isWithOrderByRank($self)
+
+Returns a true or a false value, indicating if valuation should order by rule rank or not, respectively. Default is a true value.
+
+=cut
+
 sub isWithOrderByRank  { 1 }  # When there is the rank adverb: order by rank ?
+
+=head3 isWithAmbiguous($self)
+
+Returns a true or a false value, indicating if valuation should allow ambiguous parse tree or not, respectively. Default is a false value.
+
+=cut
+
 sub isWithAmbiguous    { 0 }  # Allow ambiguous parse ?
+
+=head3 isWithNull($self)
+
+Returns a true or a false value, indicating if valuation should allow a null parse tree or not, respectively. Default is a false value.
+
+=cut
+
 sub isWithNull         { 0 }  # Allow null parse ?
+
+=head3 maxParses($self)
+
+Returns the number of maximum parse tree valuations. Default is unlimited (i.e. a false value).
+
+=cut
+
 sub maxParses          { 0 }  # Maximum number of parse tree values
+
+=head3 getResult($self)
+
+Returns the current parse tree value.
+
+=cut
+
+sub getResult { $_[0]->[0] }
+
+=head3 setResult($self)
+
+Sets the current parse tree value.
+
+=cut
+
+sub setResult { $_[0]->[0] = $_[1] }
 
 # ----------------
 # Specific actions
 # ----------------
+
+=head2 Specific actions
+
+=head3 empty_string($self)
+
+Action for rule C<chars ::=>.
+
+=cut
+
 sub empty_string            { ''               } # chars ::=
+
+=head3 hex2codepoint_character($self)
+
+Action for rule C<char ::= '\\' 'u' /[[:xdigit:]]{4}/>.
+
+=cut
+
 sub hex2codepoint_character { chr(hex($_[3]))  } # char  ::= '\\' 'u' /[[:xdigit:]]{4}/
+
+=head3 pairs($self)
+
+Action for rule C<cpairs ::= string ':' value>.
+
+=cut
+
 sub pairs                   { [ $_[1], $_[3] ] } # pairs ::= string ':' value
+
+=head3 backspace_character($self)
+
+Action for rule C<char ::= '\\' 'b'>.
+
+=cut
+
 sub backspace_character     { $_BACKSPACE      } # char  ::= '\\' 'b'
+
+=head3 formfeed_character($self)
+
+Action for rule C<char ::= '\\' 'f'>.
+
+=cut
+
 sub formfeed_character      { $_FORMFEED       } # char  ::= '\\' 'f'
+
+=head3 newline_character($self)
+
+Action for rule C<char ::= '\\' 'n'>.
+
+=cut
+
 sub newline_character       { $_NEWLINE        } # char  ::= '\\' 'n'
+
+=head3 return_character($self)
+
+Action for rule C<char ::= '\\' 'r'>.
+
+=cut
+
 sub return_character        { $_RETURN         } # char  ::= '\\' 'r'
+
+=head3 tabulation_character($self)
+
+Action for rule C<char ::= '\\' 't'>.
+
+=cut
+
 sub tabulation_character    { $_TAB            } # char  ::= '\\' 't'
 #
 # Methods that need some hacking -;
@@ -48,6 +180,13 @@ sub tabulation_character    { $_TAB            } # char  ::= '\\' 't'
 #
 # C.f. http://www.perlmonks.org/?node_id=566543 for explanation of the method
 #
+
+=head3 array_ref($self)
+
+Action for rule C<elements ::= value* separator => ','>.
+
+=cut
+
 sub array_ref {                                 # elements ::= value*
     #
     # elements ::= value+ separator => ','
@@ -57,6 +196,12 @@ sub array_ref {                                 # elements ::= value*
     #
     [ map { $_[$_*2+1] } 0..int(@_/2)-1 ]
 }
+
+=head3 members($self)
+
+Action for rule C<members  ::= pairs* separator => ','>.
+
+=cut
 
 sub members {                                   # members  ::= pairs*
     #
@@ -73,10 +218,10 @@ sub members {                                   # members  ::= pairs*
     \%hash
 }
 
-#
-# Result getter and setter
-#
-sub getResult { $_[0]->[0] }
-sub setResult { $_[0]->[0] = $_[1] }
+=head1 SEE ALSO
+
+L<MarpaX::ESLIF::ECMA404>
+
+=cut
 
 1;
