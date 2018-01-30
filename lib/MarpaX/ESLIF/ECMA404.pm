9756351577547d752c61008875e32d16a1af2a2f
Jean-Damien Durand <jeandamiendurand@free.fr>
2018-01-30 09:32:20 +0100

    use MarpaX::ESLIF 2.0.34

--- lib/MarpaX/ESLIF/ECMA404.pm
+++ lib/MarpaX/ESLIF/ECMA404.pm
@@ -28,7 +28,7 @@ This module decodes strict JSON input using L<MarpaX::ESLIF>.
 =cut
 
 use Carp qw/croak/;
-use MarpaX::ESLIF 2.0.33;   # Do not trust iconv
+use MarpaX::ESLIF 2.0.34;   # bundled libiconv
 use MarpaX::ESLIF::ECMA404::RecognizerInterface;
 use MarpaX::ESLIF::ECMA404::ValueInterface;
 use Scalar::Util qw/looks_like_number/;
